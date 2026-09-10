2190d17e4b4358af7a4e08b5431fe9816cc5f849
DammyOG7 <ibiyomidd@gmail.com>
2026-09-10 11:10:35 +0100

    Include Paystack script in RootLayout
    Added Paystack script for payment integration.

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,5 @@
 import './globals.css'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'Kontribute',
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-black min-h-screen">{children}</body>
+      <body className="bg-black min-h-screen">
+        {children}
+        <Script src="https://js.paystack.co/v1/inline.js" strategy="beforeInteractive" />
+      </body>
     </html>
   )
 }
